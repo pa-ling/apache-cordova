56f12d9b67d5aadf8122ed36d3660a61d2a1d3c4
Patrick Fehling <pa-ling@users.noreply.github.com>
2018-01-12 19:32:53 +0100

    add workshop app and new slides

---
+++ workshop/www/js/EmployeeView.js
@@ -0,0 +1,60 @@
+var EmployeeView = function(employee) {
+    
+      this.initialize = function() {
+          this.$el = $('<div/>');
+          this.$el.on('click', '.add-location-btn', this.addLocation);
+          this.$el.on('click', '.change-pic-btn', this.changePicture);
+      };
+    
+      this.render = function() {
+          this.$el.html(this.template(employee));
+          return this;
+      };
+
+      this.addLocation = function(event) {
+        event.preventDefault();
+        function gpsSuccess(position) {
+            alert('Latitude: '    + position.coords.latitude          + "\n" +
+            'Longitude: '         + position.coords.longitude         + "\n" +
+            'Altitude: '          + position.coords.altitude          + "\n" +
+            'Accuracy: '          + position.coords.accuracy          + "\n" +
+            'Timestamp: '         + position.timestamp                + "\n");
+        }
+
+        function gpsError(error) {
+            alert('code: '    + error.code    + '\n' +
+                    'message: ' + error.message + '\n');
+        }
+
+        let gpsOptions = {maximumAge: 300000, timeout: 5000, enableHighAccuracy: true};
+        navigator.geolocation.getCurrentPosition(gpsSuccess, gpsError, gpsOptions);
+        return false;
+      };
+
+      this.changePicture = function(event) {
+        event.preventDefault();
+        if (!navigator.camera) {
+            alert("Camera API not supported", "Error");
+            return;
+        }
+        var options =   {   quality: 50,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
+                            encodingType: 0     // 0=JPG 1=PNG
+                        };
+      
+        navigator.camera.getPicture(
+            function(imgData) {
+                $('.media-object', this.$el).attr('src', "data:image/jpeg;base64,"+imgData);
+            },
+            function() {
+                alert('Error taking picture', 'Error');
+            },
+            options);
+      
+        return false;
+      };
+    
+      this.initialize();
+    
+}
